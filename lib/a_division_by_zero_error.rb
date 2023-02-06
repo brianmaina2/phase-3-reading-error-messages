b7652f0d074465ecafa8f3c7521d01a1137553f1
brian.maina@student.moringaschool.com <brianmaina2>
2023-02-06 23:02:59 +0300

    completed

--- lib/a_division_by_zero_error.rb
+++ lib/a_division_by_zero_error.rb
@@ -1,3 +1,5 @@
-42 / 6
-
-42 / 0
+begin
+    result = 42 / 0
+  rescue ZeroDivisionError
+    puts "Cannot divide by zero"
+  end
--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -1,3 +1,8 @@
 1 + 1
+#=> 2
 
-1 + "is the loneliest number"
+1 + "1".to_i
+#=> 2
+
+1.0 + "1.0".to_f
+#=> 2.0
